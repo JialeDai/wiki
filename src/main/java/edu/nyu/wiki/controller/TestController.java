c58e726e71ea548cffba006923d542dba6c50d74
JialeDai <jd4678@nyu.edu>
2021-08-21 16:26:04 +0800

    add test script

--- src/main/java/edu/nyu/wiki/controller/TestController.java
+++ src/main/java/edu/nyu/wiki/controller/TestController.java
@@ -1,12 +1,21 @@
 package edu.nyu.wiki.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
-        return "hello world";
+        return "hello world: get";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(String name){
+        return "hello world: post -> " + name;
     }
 }
