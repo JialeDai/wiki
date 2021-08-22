5be0f50bda804d4f0a851621baf6af273fc1b10e
JialeDai <jd4678@nyu.edu>
2021-08-22 14:52:55 +0800

    add commonResp

--- src/main/java/edu/nyu/wiki/controller/EbookController.java
+++ src/main/java/edu/nyu/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package edu.nyu.wiki.controller;
 
 import edu.nyu.wiki.model.Ebook;
+import edu.nyu.wiki.resp.CommonResp;
 import edu.nyu.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,10 @@ public class EbookController {
     private EbookService EbookService;
 
     @GetMapping("/ebook/list")
-    public List<Ebook> list(){
-        return EbookService.list();
+    public CommonResp<List<Ebook>> list(){
+        List<Ebook> list = EbookService.list();
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        resp.setContent(list);
+        return resp;
     }
 }
