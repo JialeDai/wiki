d51efb5e801ba3dcdb68baeed281ff4590af3212
JialeDai <jd4678@nyu.edu>
2021-08-22 17:02:03 +0800

    add copyutils

--- src/main/java/edu/nyu/wiki/service/EbookService.java
+++ src/main/java/edu/nyu/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import edu.nyu.wiki.mapper.EbookMapper;
 import edu.nyu.wiki.model.Ebook;
 import edu.nyu.wiki.model.EbookExample;
 import edu.nyu.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import edu.nyu.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,21 +16,12 @@ public class EbookService {
     @Resource
     private EbookMapper EbookMapper;
 
-//    public List<Ebook> list() {
-//        return EbookMapper.selectByExample(null);
-//    }
-
     public List<EbookResp> list(EbookReq ebookReq) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%"+ebookReq.getName()+"%");
         List<Ebook> ebookList = EbookMapper.selectByExample(ebookExample);
-        List<EbookResp> ebookRespList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            ebookRespList.add(ebookResp);
-        }
+        List<EbookResp> ebookRespList = CopyUtil.copyList(ebookList, EbookResp.class);
         return ebookRespList;
     }
 }
