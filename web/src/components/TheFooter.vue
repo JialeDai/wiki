c35eb985ad11e4ad65453282787f00cca15bb942
JialeDai <jd4678@nyu.edu>
2021-08-23 23:35:58 +0800

    install axios

---
+++ web/src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    派大星电子书11
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+});
+</script>
