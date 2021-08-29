80d861b23cc099b365b0046ae0a59e108c13e454
jiale <jd4678@nyu.edu>
2021-08-29 15:48:23 +0800

    增加分页列表

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -11,7 +11,7 @@
           <a-sub-menu key="sub1">
             <template #title>
               <span>
-                <user-outlined />
+                <user-outlined/>
                 subnav 1
               </span>
             </template>
@@ -23,7 +23,7 @@
           <a-sub-menu key="sub2">
             <template #title>
               <span>
-                <laptop-outlined />
+                <laptop-outlined/>
                 subnav 2
               </span>
             </template>
@@ -35,7 +35,7 @@
           <a-sub-menu key="sub3">
             <template #title>
               <span>
-                <notification-outlined />
+                <notification-outlined/>
                 subnav 3
               </span>
             </template>
@@ -49,18 +49,63 @@
       <a-layout-content
           :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
       >
-        <pre>
-{{ebooks}}}
-        </pre>
+        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+          <template #footer>
+            <div>
+              <b>ant design vue</b>
+              footer part
+            </div>
+          </template>
+          <template #renderItem="{ item }">
+            <a-list-item key="item.title">
+              <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px"/>
+            {{ text }}
+          </span>
+              </template>
+              <template #extra>
+                <img
+                    width="272"
+                    alt="logo"
+                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                />
+              </template>
+              <a-list-item-meta :description="item.description">
+                <template #title>
+                  <a :href="item.href">{{ item.title }}</a>
+                </template>
+                <template #avatar>
+                  <a-avatar :src="item.avatar"/>
+                </template>
+              </a-list-item-meta>
+              {{ item.content }}
+            </a-list-item>
+          </template>
+        </a-list>
       </a-layout-content>
     </a-layout>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 import axios from 'axios'
+
+const listData: Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
 
 export default defineComponent({
   name: 'Home',
@@ -68,10 +113,10 @@ export default defineComponent({
     console.log("set up");
     const ebooks = ref()
 
-    onMounted(()=>{
+    onMounted(() => {
       console.log("onMounted")
       axios.get("http://localhost:8880/ebook/list?name=Python").then(
-          (response) =>{
+          (response) => {
             console.log(response)
             const data = response.data
             ebooks.value = data.content
@@ -79,7 +124,19 @@ export default defineComponent({
     })
 
     return {
-      ebooks
+      ebooks,
+      listData,
+      pagination:{
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3
+      },
+      actions: [
+        {type : "StarOutLined", text: "156"},
+        {type : "LikeOutLined", text: "156"},
+        {type: "MessageOutlined", text: 2}
+      ],
     }
   }
 });
